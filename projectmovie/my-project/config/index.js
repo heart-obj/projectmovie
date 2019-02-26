c9d7f7e670dfbf0ecfd2307adf3b5159a38cbe71
newllb <1135769417@qq.com>
2019-02-26 17:59:56 +0800

    movie电影项目

--- projectmovie/my-project/config/index.js
+++ projectmovie/my-project/config/index.js
@@ -10,7 +10,23 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api1':{
+        target:"http://localhost:8080",
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api1':''
+        }
+      },
+      '/api2':{
+        target:"http://m.maoyan.com",
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api2':''
+        }
+      },
+      
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- projectmovie/my-project/src/components/movielist.vue
+++ projectmovie/my-project/src/components/movielist.vue
@@ -2,12 +2,36 @@
     <div class="list-box">
         <div>
             <el-row v-for='(val,i) in datalist' v-bind:key="i">
-                <el-col class="list-t">
+                <!-- <el-col class="list-t">
                     <span>{{val.title}}</span>
                     <span class="iconfont icon-ios-arrow-forward"></span>
-                </el-col>
-                <el-col class="moive-r">
-                    <div class=""></div>
+                </el-col> -->
+                <el-col class="movie-r">
+                    <div class="movie-l">
+                        <div class="movie-img">
+                            <img src="https://p0.meituan.net/128.180/movie/fc4dd6cd0c6f7db566a128cc05c475355664427.jpg" alt="">
+                        </div>
+                        <div class="movie-t">
+                            <div class="movie-introduce">
+                                <h3>决战食神</h3>
+                                <div class="movie-rate">
+                                    <el-rate
+                                        v-bind:value=3.5
+                                        disabled
+                                        show-score
+                                        text-color="#ff9900"
+                                        score-template="">
+                                    </el-rate>
+                                </div>
+                                <div class="text-d">今天199家影院放映3380场</div>
+                                <div class="text-d">主演：罗莎·萨拉查,克里斯托弗·沃尔兹,基恩·约翰逊</div>
+                            </div>
+                            <div class="">
+
+                            </div>
+                        </div>
+                        
+                    </div>
                     <div></div>
                 </el-col>
             </el-row>
@@ -28,10 +52,10 @@ export default {
         let _this=this;
         this.$http({
             method: 'get',
-            url:'static/moviedata.json',
+            url:'/api2/ajax/movieOnInfoList?token=',
         })
         .then((response)=>{
-            console.log(response)
+            console.log(response.data.movieList)
             _this.datalist=response.data;
         })
         .catch((error)=>{
@@ -58,11 +82,51 @@ export default {
 .list-t span:nth-child(2){
     float:right;
 }
-.moive-r{
+.movie-r{
     width: 100%;
     height: 50.2vw;
     box-shadow: 0 0 8px rgba(0, 0, 0, .2);
     border-radius: 5px;
-    padding: 20px;
+    padding: 10px;
+}
+.movie-l{
+    width: 100%;
+    height: 100%;
+}
+.movie-img{
+    width: 28vw;
+    height: 100%;
+    float: left;
+}
+.movie-img img{
+    width: 100%;
+    height: 100%;
+}
+.movie-t{
+    width: calc(100% - 28vw);
+    height: 100%;
+    float: left;
+    padding-left: 10px;
+    box-sizing: border-box;
+}
+.movie-introduce{
+    max-width:100%;
+    height: 100%;
+}
+.movie-introduce h3{
+    font-size: 4.45vw;
+    text-align: left;
+}
+.movie-rate{
+    text-align: left;
+}
+.movie-introduce .text-d{
+    width: 100%;
+    height: 7vw;
+    overflow: hidden;
+    text-align: left;
+    font-size: 3.34vw;
+    line-height: 3.34vw;
+    margin-top: 10px;
 }
 </style>
